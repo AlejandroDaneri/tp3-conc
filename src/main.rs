ae5ba806eddf8ce293071c1c50f92ffde4af5c88
Matías Lafroce <mlafroce@gmail.com>
2021-07-10 18:50:33 -0300

    Agrega draft de cliente

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::process;
+use tp3::blockchain::client::Client;
+
+fn main() -> std::io::Result<()> {
+    let port_from = 9000;
+    let port_to = 9003;
+    let id = process::id();
+    let client = Client::new(id);
+    client.run(port_from, port_to)
 }
