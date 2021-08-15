262c10f3226f4eb6fbe71ea166273e8bb95a0693
Matías Lafroce <mlafroce@gmail.com>
2021-08-15 14:00:13 -0300

    Fixes en log

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,20 @@
 #[allow(unused_macros)]
 pub mod macros {
     macro_rules! debug {
-        ($fmt:expr) => (println!(concat!("\x1B[2m[DEBUG]\x1B[0m ", file!(), ":", line!(), " ", $fmt)));
-        ($fmt:expr, $($arg:tt)*) => (println!(concat!("\x1B[2m[DEBUG]\x1B[0m ", file!(), ":", line!(), " ", $fmt), $($arg)*));
+        ($fmt:expr) => (println!(concat!("\x1B[2m[DEBUG]\x1B[0m(", file!(), ":", line!(), ")\x1B[0m ", $fmt)));
+        ($fmt:expr, $($arg:tt)*) => (println!(concat!("\x1B[2m[DEBUG]\x1B[0m(", file!(), ":", line!(), ")\x1B[0m ", $fmt), $($arg)*));
     }
     macro_rules! info {
-        ($fmt:expr) => (println!(concat!("\x1B[92m[INFO ]\x1B[0m ", file!(), ":", line!(), " ", $fmt)));
-        ($fmt:expr, $($arg:tt)*) => (println!(concat!("\x1B[92m[INFO ]\x1B[0m ", file!(), ":", line!(), " ", $fmt), $($arg)*));
+        ($fmt:expr) => (println!(concat!("\x1B[92m[INFO ]\x1B[0m(", file!(), ":", line!(), ")\x1B[0m ", $fmt)));
+        ($fmt:expr, $($arg:tt)*) => (println!(concat!("\x1B[92m[INFO ]\x1B[0m(", file!(), ":", line!(), ")\x1B[0m ", $fmt), $($arg)*));
     }
     macro_rules! warn {
-        ($fmt:expr) => (println!(concat!("\x1B[93m[WARN ]\x1B[0m ", file!(), ":", line!(), " ", $fmt)));
-        ($fmt:expr, $($arg:tt)*) => (println!(concat!("\x1B[93m[WARN ]\x1B[0m ", file!(), ":", line!(), " ", $fmt), $($arg)*));
+        ($fmt:expr) => (println!(concat!("\x1B[93m[WARN ]\x1B[0m(", file!(), ":", line!(), ")\x1B[0m ", $fmt)));
+        ($fmt:expr, $($arg:tt)*) => (println!(concat!("\x1B[93m[WARN ]\x1B[0m(", file!(), ":", line!(), ")\x1B[0m ", $fmt), $($arg)*));
     }
     macro_rules! error {
-        ($fmt:expr) => (println!(concat!("\x1B[91m[ERROR]\x1B[0m ", file!(), ":", line!(), " ", $fmt)));
-        ($fmt:expr, $($arg:tt)*) => (println!(concat!("\x1B[91m[ERROR]\x1B[0m ", file!(), ":", line!(), " ", $fmt), $($arg)*));
+        ($fmt:expr) => (println!(concat!("\x1B[91m[ERROR]\x1B[0m(", file!(), ":", line!(), ")\x1B[0m ", $fmt)));
+        ($fmt:expr, $($arg:tt)*) => (println!(concat!("\x1B[91m[ERROR]\x1B[0m(", file!(), ":", line!(), ")\x1B[0m ", $fmt), $($arg)*));
     }
 }
 
